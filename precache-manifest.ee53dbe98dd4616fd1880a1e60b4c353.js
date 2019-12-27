8ca01447cecf68f897fa0dbc2656b0444269f321
Israel Torres <israel.a.torres01@gmail.com>
2019-12-26 21:42:57 -0500

    Updates

---
+++ precache-manifest.ee53dbe98dd4616fd1880a1e60b4c353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797d65c37b20587616b74d4d766b992b",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "a8f4ad7358030ac73c5c",
+    "url": "/Monsters-Rolodex/static/css/main.89be679a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/Monsters-Rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8f4ad7358030ac73c5c",
+    "url": "/Monsters-Rolodex/static/js/main.78319752.chunk.js"
+  },
+  {
+    "revision": "29f656592ba48eaf223e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.166da6c4.js"
+  }
+]);
